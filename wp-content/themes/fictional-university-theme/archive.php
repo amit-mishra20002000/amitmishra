a875631a18df049dd66d0c646a93770ea8902f47
amit-mishra20002000 <amit.mishra20002000@gmail.com>
2024-03-06 23:58:20 +0530

    section-6

--- wp-content/themes/fictional-university-theme/archive.php
+++ wp-content/themes/fictional-university-theme/archive.php
@@ -2,7 +2,16 @@
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg'); ?>)"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Blog</h1>
+    <h1 class="headline headline--large">
+      <?php if(is_category()){
+        single_cat_title();
+      }
+      if(is_author()){
+        echo 'Posted By '.get_the_author();
+      }
+      
+      ?>
+    </h1>
     <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
     <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
     <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
